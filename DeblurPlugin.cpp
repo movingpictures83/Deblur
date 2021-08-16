2f4e3cba54bbab715e766d9dbbb34e7b6e625017
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-08-15 21:09:49 -0400

    Moved conda

--- DeblurPlugin.cpp
+++ DeblurPlugin.cpp
@@ -23,7 +23,11 @@ void DeblurPlugin::output(std::string file) {
    command += "export PATH=${CONDA_HOME}/bin/:${PATH}; ";	
    command += "eval \"$(conda shell.bash hook)\"; ";
    command += "conda activate qiime2-2021.4; ";
-   command += "qiime deblur denoise-16S --i-demultiplexed-seqs "+std::string(PluginManager::prefix())+"/"+parameters["inputfile"]+" --p-trim-length "+parameters["trimlength"]+" --o-representative-sequences "+file+"-rep-seqs.qza --o-table "+file+"-table.qza --p-sample-stats --o-stats "+file+"-deblur-stats.qza; ";
+   command += "qiime deblur denoise-16S --i-demultiplexed-seqs "+std::string(PluginManager::prefix())+"/"+parameters["inputfile"]+" --p-trim-length "+parameters["trimlength"]+" --p-left-trim-len "+parameters["lefttrimlen"];
+   if (parameters.count("meanerror") != 0)
+	   command += " --p-mean-error "+parameters["meanerror"];
+
+   command += " --o-representative-sequences "+file+"-rep-seqs.qza --o-table "+file+"-table.qza --p-sample-stats --o-stats "+file+"-deblur-stats.qza; ";
    command += "conda deactivate; ";
    command += "conda deactivate; ";
    command += "export PATH=${OLDPATH}";
